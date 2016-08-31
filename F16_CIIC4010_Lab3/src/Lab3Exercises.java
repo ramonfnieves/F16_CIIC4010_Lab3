e427a889e5e8d6ddc58d359e7fa1fe694f82bfaf
s802975159 <s802975159@papaya.ece.uprm.edu>
2016-08-30 23:12:29 -0400

    Lab3 - Commit and Push

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
+		
 		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setTitle("Ramon Nieves");
+		myFrame.setLocation(800, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
