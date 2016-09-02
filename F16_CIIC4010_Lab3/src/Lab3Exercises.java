e6e976f474a9f0e1b120030015d3941e658a51e5
s802975159 <s802975159@toronja.ece.uprm.edu>
2016-09-01 20:35:57 -0400

    Lab3

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,8 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		
+
+	
 		JFrame myFrame = new JFrame("Hello, world!");
+
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
 		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setTitle("Ramon Nieves");
---
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -0,0 +1,72 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Polygon;
+
+import javax.swing.JPanel;
+
+public class MyPanelClass extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		//Compute interior coordinates
+		Insets myInsets = this.getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		int x2 = getWidth() - myInsets.right - 1;
+		int y2 = getHeight() - myInsets.bottom - 1;
+
+		int ovalWidth =55;
+		int ovalHeigth =55;
+		
+		int width = x2 - x1;
+		int height = y2 - y1;
+
+		//Paint the background
+		g.setColor(Color.BLUE);
+		g.fillRect(x1+1, y1+1, width-1, height-1);
+/*
+		g.setColor(Color.YELLOW);
+		g.drawRect(x1+10, y1+10, width-20, height-20);
+
+		g.setColor(Color.WHITE);
+		g.drawLine(x1, y1, x2, y2);
+
+		g.setColor(Color.MAGENTA);
+		g.drawLine(getWidth(), 0, 0,getHeight());
+
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillOval(getWidth()/2-ovalWidth/2, getHeight()/2 - ovalHeigth/2, ovalWidth, ovalHeigth);
+		
+
+		Polygon p = new Polygon();
+        p.addPoint(x1 + 5, y1 + 25);
+        p.addPoint(x1 + 20, y1 + 10);
+        p.addPoint(x1 + 35, y1 + 25);
+        p.addPoint(x1 + 25, y1 + 25);
+        p.addPoint(x1 + 25, y1 + 45);
+        p.addPoint(x1 + 15, y1 + 45);
+        p.addPoint(x1 + 15, y1 + 25);
+        g.setColor(Color.YELLOW);
+        g.fillPolygon(p);*/
+        
+        Polygon p2 = new Polygon();
+        p2.addPoint(x1 + 25, y1 + 73);
+        p2.addPoint(x1 + 41, y1 + 73);
+        p2.addPoint(x1 + 47, y1 + 58);
+        p2.addPoint(x1 + 53, y1 + 73);
+        p2.addPoint(x1 + 69, y1 + 73);
+        p2.addPoint(x1 + 56, y1 + 83);
+        p2.addPoint(x1 + 61, y1 + 98);
+        p2.addPoint(x1 + 47, y1 + 88);
+        p2.addPoint(x1 + 34, y1 + 98);
+        p2.addPoint(x1 + 38, y1 + 83);
+        g.setColor(Color.WHITE);
+        g.drawPolygon(p2);
+	}
+}
